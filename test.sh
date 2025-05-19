f9766d350c737a1a0fd85c13a9c8cd4a2dbdf0af
Nail Dehmej <ndehmej@k1r4p3.42mulhouse.fr>
2025-05-19 03:26:58 +0200

    ya presque tous qui marche2, verifier pq il canne a 5 800 200 200

--- test.sh
+++
@@ -1,7 +0,0 @@
-grep -r "int check_args" srcs
-grep -r "int init_data" srcs
-grep -r "void create_threads" srcs
-grep -r "int all_eat" srcs
-grep -r "int destroy_simu" srcs
-grep -r "void print_statut" srcs
-grep -r "void philo_eat" srcs
